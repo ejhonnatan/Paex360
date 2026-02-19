a0c4692670c55704deea2889b8e63729f54887f6
Jhonnatan Echeverri <73188815+ejhonnatan@users.noreply.github.com>
2026-02-18 23:47:37 -0500

    Add getConfig function for Firebase configuration
    Create a Netlify function to return Firebase config.

---
+++ netlify/functions/getConfig.js
@@ -0,0 +1,31 @@
+// netlify/functions/getConfig.js
+
+exports.handler = async () => {
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID
+      })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        error: err.message
+      })
+    };
+  }
+};
